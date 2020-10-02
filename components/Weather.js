27b00e7b70cf5a1c0c4005f2750c38dac6943406
6135512065 <lan_6744@hotmail.com>
2020-10-02 16:18:31 +0700

    Navigation&RouteParameter

--- components/Weather.js
+++ components/Weather.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, ImageBackground, StyleSheet } from 'react-native';
 import Forecast from './Forecast';
 
+
 const apiKey = '6cecb9dd2e369d7e9b5d62bc682150d4'
 
 export default function Weather(props) {
@@ -32,34 +33,35 @@ export default function Weather(props) {
         }
     }, [props.zipCode])
 
+
     return (
         <View>
             <ImageBackground source={require('./bg.jpg')} style={styles.backdrop}>
                 <View style={styles.cover}>
-                    <Text style={styles.medium}>Zip Code is :  {props.zipCode}</Text>
+                    <Text style={styles.medium}>Zip Code:  {props.zipCode}</Text>
                     <Forecast {...forecastInfo} />
                 </View>
             </ImageBackground>
         </View>
     );
 }
-
 const styles = StyleSheet.create({
     backdrop: {
         alignItems: 'center',
+
         width: '100%',
         height: '100%'
     },
     cover: {
         backgroundColor: 'black',
         width: '100%',
-        height: 260,
+        height: 250,
         opacity: 0.4,
         alignItems: 'center',
     },
     medium: {
         marginTop: 32,
-        fontSize: 20,
+        fontSize: 15,
         color: 'white',
     }
 });
