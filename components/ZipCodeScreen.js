808f5fab4cbdb8f3cb7a1265dd21a9f505f88f11
6135512065 <lan_6744@hotmail.com>
2020-10-02 16:23:20 +0700

    ปรับปรุงWeatherApp ( เพิ่มจังหวัด )

--- components/ZipCodeScreen.js
+++ components/ZipCodeScreen.js
@@ -4,11 +4,20 @@ import { View, Text, FlatList, TouchableHighlight } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const availableZipItems = [
-    { place: 'Hatyai', code: '90110' },
-    { place: 'Trang', code: '92000' },
-    { place: 'Chiangmai', code: '50000' },
-    { place: 'Khonkaen', code: '40000' },
-    { place: 'Chonburi', code: '20000' },
+    { place: '1. Nakhonsithammarat', code: '80000' },
+    { place: '2. Krabi', code: '81000' },
+    { place: '3. Pangnga', code: '82000' },
+    { place: '4. Phuket', code: '83000' },
+    { place: '5. Suratthani', code: '84000' },
+    { place: '6. Ranong', code: '85000' },
+    { place: '7. Chumporn', code: '86000' },
+    { place: '8. Songkhla', code: '90000' },
+    { place: '9. Satun', code: '91000' },
+    { place: '10. Trang', code: '92000' },
+    { place: '11. Phatthalung', code: '93000' },
+    { place: '12. Pattani', code: '94000' },
+    { place: '13. Yala', code: '95000' },
+    { place: '14. Narathiwat', code: '96000' },
 ]
 
 const ZipItem = ({ place, code, navigation }) => (
